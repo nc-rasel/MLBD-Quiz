bad742ca81a713517bf0f8cca2ac7d9bc2d1441f
Mahamud Hasan Rashel <m.hasan@sekai-lab-bd.com>
2020-03-27 21:09:04 +0600

    added post type Event and questions

---
+++ README.md
@@ -0,0 +1,10 @@
+Here how you can run mlbd-quiz theme: 
+---
+Make sure you have install node, composer, webserver, php, mysql or relative application installed on your machine.
+```sh
+create a database
+edit "wp-config-sample.ph" to "wp-config.php" and write the information
+Inside of "wp-content/themes/mlbd-quiz" folder run the following command
+composer install
+yarn
+yarn run dev
--- wp-content/themes/mlbd-quiz/app/Structure/posttypes.php
+++ wp-content/themes/mlbd-quiz/app/Structure/posttypes.php
@@ -16,32 +16,64 @@ namespace Tonik\Theme\App\Structure;
 use function Tonik\Theme\App\config;
 
 /**
- * Registers `book` custom post type.
+ * Registers `Event` custom post type.
  *
  * @return void
  */
-function register_book_post_type()
+function register_event_post_type()
 {
-    register_post_type('book', [
-        'description' => __('Collection of books.', config('textdomain')),
+    register_post_type('event', [
+        'description' => __('Event.', config('textdomain')),
         'public' => true,
-        'supports' => ['title', 'editor', 'excerpt', 'thumbnail'],
+        'supports' => ['title', 'editor'],
         'labels' => [
-            'name' => _x('Books', 'post type general name', config('textdomain')),
-            'singular_name' => _x('Book', 'post type singular name', config('textdomain')),
-            'menu_name' => _x('Books', 'admin menu', config('textdomain')),
-            'name_admin_bar' => _x('Book', 'add new on admin bar', config('textdomain')),
-            'add_new' => _x('Add New', 'book', config('textdomain')),
-            'add_new_item' => __('Add New Book', config('textdomain')),
-            'new_item' => __('New Book', config('textdomain')),
-            'edit_item' => __('Edit Book', config('textdomain')),
-            'view_item' => __('View Book', config('textdomain')),
-            'all_items' => __('All Books', config('textdomain')),
-            'search_items' => __('Search Books', config('textdomain')),
-            'parent_item_colon' => __('Parent Books:', config('textdomain')),
-            'not_found' => __('No books found.', config('textdomain')),
-            'not_found_in_trash' => __('No books found in Trash.', config('textdomain')),
+            'name' => _x('Event', 'post type general name', config('textdomain')),
+            'singular_name' => _x('Event', 'post type singular name', config('textdomain')),
+            'menu_name' => _x('Event', 'admin menu', config('textdomain')),
+            'name_admin_bar' => _x('Event', 'add new on admin bar', config('textdomain')),
+            'add_new' => _x('Add New', 'event', config('textdomain')),
+            'add_new_item' => __('Add New Event', config('textdomain')),
+            'new_item' => __('New Event', config('textdomain')),
+            'edit_item' => __('Edit Event', config('textdomain')),
+            'view_item' => __('View Event', config('textdomain')),
+            'all_items' => __('All Event', config('textdomain')),
+            'search_items' => __('Search Event', config('textdomain')),
+            'parent_item_colon' => __('Parent Event:', config('textdomain')),
+            'not_found' => __('No Event found.', config('textdomain')),
+            'not_found_in_trash' => __('No Event found in Trash.', config('textdomain')),
         ],
     ]);
 }
-add_action('init', 'Tonik\Theme\App\Structure\register_book_post_type');
+add_action('init', 'Tonik\Theme\App\Structure\register_event_post_type');
+
+
+/**
+ * Registers `Question` custom post type.
+ *
+ * @return void
+ */
+function register_question_post_type()
+{
+    register_post_type('Question', [
+        'description' => __('Question.', config('textdomain')),
+        'public' => true,
+        'supports' => ['title', 'editor'],
+        'labels' => [
+            'name' => _x('Question', 'post type general name', config('textdomain')),
+            'singular_name' => _x('Question', 'post type singular name', config('textdomain')),
+            'menu_name' => _x('Question', 'admin menu', config('textdomain')),
+            'name_admin_bar' => _x('Question', 'add new on admin bar', config('textdomain')),
+            'add_new' => _x('Add New', 'Question', config('textdomain')),
+            'add_new_item' => __('Add New Question', config('textdomain')),
+            'new_item' => __('New Question', config('textdomain')),
+            'edit_item' => __('Edit Question', config('textdomain')),
+            'view_item' => __('View Question', config('textdomain')),
+            'all_items' => __('All Question', config('textdomain')),
+            'search_items' => __('Search Question', config('textdomain')),
+            'parent_item_colon' => __('Parent Question:', config('textdomain')),
+            'not_found' => __('No Question found.', config('textdomain')),
+            'not_found_in_trash' => __('No Question found in Trash.', config('textdomain')),
+        ],
+    ]);
+}
+add_action('init', 'Tonik\Theme\App\Structure\register_question_post_type');
