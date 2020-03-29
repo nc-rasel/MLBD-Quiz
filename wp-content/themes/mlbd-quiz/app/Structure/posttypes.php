b47fadd772a13b9cd291cc8e920e0c21b19e3a88
mustafa-Mlab <mustofa.kamal@sekai-lab-bd.com>
2020-03-29 12:53:50 +0600

    feat: added custom admin page

--- wp-content/themes/mlbd-quiz/app/Structure/posttypes.php
+++ wp-content/themes/mlbd-quiz/app/Structure/posttypes.php
@@ -52,28 +52,28 @@ add_action('init', 'Tonik\Theme\App\Structure\register_event_post_type');
  *
  * @return void
  */
-function register_question_post_type()
-{
-    register_post_type('Question', [
-        'description' => __('Question.', config('textdomain')),
-        'public' => true,
-        'supports' => ['title', 'editor'],
-        'labels' => [
-            'name' => _x('Question', 'post type general name', config('textdomain')),
-            'singular_name' => _x('Question', 'post type singular name', config('textdomain')),
-            'menu_name' => _x('Question', 'admin menu', config('textdomain')),
-            'name_admin_bar' => _x('Question', 'add new on admin bar', config('textdomain')),
-            'add_new' => _x('Add New', 'Question', config('textdomain')),
-            'add_new_item' => __('Add New Question', config('textdomain')),
-            'new_item' => __('New Question', config('textdomain')),
-            'edit_item' => __('Edit Question', config('textdomain')),
-            'view_item' => __('View Question', config('textdomain')),
-            'all_items' => __('All Question', config('textdomain')),
-            'search_items' => __('Search Question', config('textdomain')),
-            'parent_item_colon' => __('Parent Question:', config('textdomain')),
-            'not_found' => __('No Question found.', config('textdomain')),
-            'not_found_in_trash' => __('No Question found in Trash.', config('textdomain')),
-        ],
-    ]);
-}
-add_action('init', 'Tonik\Theme\App\Structure\register_question_post_type');
+// function register_question_post_type()
+// {
+//     register_post_type('Question', [
+//         'description' => __('Question.', config('textdomain')),
+//         'public' => true,
+//         'supports' => ['title', 'editor'],
+//         'labels' => [
+//             'name' => _x('Question', 'post type general name', config('textdomain')),
+//             'singular_name' => _x('Question', 'post type singular name', config('textdomain')),
+//             'menu_name' => _x('Question', 'admin menu', config('textdomain')),
+//             'name_admin_bar' => _x('Question', 'add new on admin bar', config('textdomain')),
+//             'add_new' => _x('Add New', 'Question', config('textdomain')),
+//             'add_new_item' => __('Add New Question', config('textdomain')),
+//             'new_item' => __('New Question', config('textdomain')),
+//             'edit_item' => __('Edit Question', config('textdomain')),
+//             'view_item' => __('View Question', config('textdomain')),
+//             'all_items' => __('All Question', config('textdomain')),
+//             'search_items' => __('Search Question', config('textdomain')),
+//             'parent_item_colon' => __('Parent Question:', config('textdomain')),
+//             'not_found' => __('No Question found.', config('textdomain')),
+//             'not_found_in_trash' => __('No Question found in Trash.', config('textdomain')),
+//         ],
+//     ]);
+// }
+// add_action('init', 'Tonik\Theme\App\Structure\register_question_post_type');
