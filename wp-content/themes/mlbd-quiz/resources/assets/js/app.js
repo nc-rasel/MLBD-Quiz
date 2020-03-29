efc9f53d1bf4390df9b75c2b2ae9fb27b2330b00
mustafa-Mlab <mustofa.kamal@sekai-lab-bd.com>
2020-03-29 23:35:14 +0600

    fix: event form validation

--- wp-content/themes/mlbd-quiz/resources/assets/js/app.js
+++ wp-content/themes/mlbd-quiz/resources/assets/js/app.js
@@ -5,46 +5,60 @@ import Cookie from './_cookie';
 const cookie = new Cookie();
 
 $(document).ready(function () {
-  if(cookie.read('username')){
+  if (cookie.read('username')) {
     $(".welcome-popup-wrapper").remove();
   }
   $("#welcome-form #submit").on("click", function (e) {
     e.preventDefault();
     var username = $("#welcome-form").find("#user_name").val();
-    if(username){
+    if (username) {
       $('.error-message').hide();
       cookie.remove('username');
       cookie.create('username', username, 365);
       $(".welcome-popup-wrapper").remove();
-    }else{
+    } else {
       $('.error-message').show();
     }
   });
   $("#answer_form #submit").on("click", function (e) {
     e.preventDefault();
+    // validation
     var postId = $('article').data("post-id");
     var form = $("#answer_form");
+    let valid = 1;
     $(form).find(".form-group").each(function (index, value) {
-      let metaID = $(value).data("id");
       $(value).find(".answer_wrapper").each(function (id, data) {
         var answer = $.trim($(data).find("textarea").val());
-        var anynomus = ($(data).find("input[type='checkbox']").prop("checked")) ? 1 : 0;
-        const dataVariables = {
-          'action': 'update_question_answer',
-          'meataID': metaID,
-          'answer': answer,
-          'anynomus': anynomus,
-          'name': cookie.read('username'),
-          'postID': postId,
-        };
-        // Captcha Test ajax call
-        $.post(vars.ajaxurl, dataVariables).then(response => {
-          if(response){
-            window.location.href= vars.home_url + '/thank-you';
-          }
-        })
+        if (!answer.trim()) { valid = 0; 
+          $(data).append('<div class="error-message" style="display:block">Answer field is required</div>');
+        }
       });
     });
+    console.log(valid);
+    if (valid == 1) {
+      $(form).find(".form-group").each(function (index, value) {
+        let metaID = $(value).data("id");
+        $(value).find(".answer_wrapper").each(function (id, data) {
+          var answer = $.trim($(data).find("textarea").val());
+          var anynomus = ($(data).find("input[type='checkbox']").prop("checked")) ? 1 : 0;
+          const dataVariables = {
+            'action': 'update_question_answer',
+            'meataID': metaID,
+            'answer': answer,
+            'anynomus': anynomus,
+            'name': cookie.read('username'),
+            'postID': postId,
+          };
+          // Captcha Test ajax call
+          $.post(vars.ajaxurl, dataVariables).then(response => {
+            if (response) {
+              // window.location.href = vars.home_url + '/thank-you';
+            }
+          })
+        });
+      });
+
+    }
   });
 
   $("body").on("click", ".add_answer", function (e) {
