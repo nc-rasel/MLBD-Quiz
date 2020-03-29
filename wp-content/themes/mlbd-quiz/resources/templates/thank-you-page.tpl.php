bf48fa89a4bbba1fd298d2acd66a956312495ce9
Mahamud Hasan Rashel <m.hasan@sekai-lab-bd.com>
2020-03-29 22:03:25 +0600

    thankyou and expire page style and formatting compplete

--- wp-content/themes/mlbd-quiz/resources/templates/thank-you-page.tpl.php
+++ wp-content/themes/mlbd-quiz/resources/templates/thank-you-page.tpl.php
@@ -1,15 +1,19 @@
 <?php get_header(); ?>
 
 <section class="thank-you">
-    <div class="section-container">
-        <div class="content">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post() ?>
-            <?php
-              the_content();
-            ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
+    <div class="wrap-thankyou">
+        <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php if (have_posts()) : ?>
+                    <?php while (have_posts()) : the_post() ?>
+                    <?php
+                    the_content();
+                    ?>
+                    <?php endwhile; ?>
+                <?php endif; ?>
+                </div>
+            </div>
         </div>
     </div>
 </section>
