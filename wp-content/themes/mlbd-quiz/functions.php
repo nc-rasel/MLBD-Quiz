cb4f3c14503cfc106150b96adf5315e0b01aa229
rasel <raselux@gmail.com>
2020-03-30 12:51:01 +0600

    Fix UI

--- wp-content/themes/mlbd-quiz/functions.php
+++ wp-content/themes/mlbd-quiz/functions.php
@@ -99,7 +99,7 @@ if( function_exists('acf_add_local_field_group') ):
                         'name' => 'description',
                         'type' => 'textarea',
                         'instructions' => '',
-                        'required' => 1,
+                        'required' => 0,
                         'conditional_logic' => 0,
                         'wrapper' => array(
                             'width' => '',
